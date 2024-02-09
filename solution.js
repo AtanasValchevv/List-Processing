68fc492fd83fb7dad63615a49a6e6fe54de6ad5c
Jorkata113 <georgi.gyurov.highschool@buditel.bg>
2024-02-09 13:55:03 +0200

    reverse sort count
    asd

--- solution.js
+++ solution.js
@@ -11,7 +11,7 @@ function processInput() {
   console.log('added event listener');
   let cmd = document.getElementById("inputBox").value;
   let result = processCommand(cmd);
-  if (result)
+  if (result !== undefined && result !== null)
     printToTerminal(result);
   printToTerminal("List: " + list.join(" "));
   document.getElementById("inputBox").value = "";
@@ -23,17 +23,24 @@ function printToTerminal(text) {
 
 function processCommand(cmd) {
   let cmdArgs = cmd.split(" ");
-  cmd = cmdArgs.shift();
-  switch (cmd) {
+  let cmdName = cmdArgs.shift();
+  switch (cmdName) {
     case "append":
       return append(cmdArgs);
       break;
 
-    // TODO: process more commands here ...
+    case "sort":
+      return sortList();
+      break;
 
-    // case "anotherCommand":
-    //   return anotherCommand(cmdArgs);
-    //   break;
+    case "count":
+      let searchString = cmdArgs.join(" ");
+      return countOccurrences(searchString);
+      break;
+
+    case "reverse":
+      return reverseList();
+      break;
 
     default:
       return "Error: invalid command";
@@ -45,15 +52,27 @@ function append(args) {
   list = list.concat(args);
 }
 
-// TODO: implement more commands here ...
 
-function sort(){
+function sortList() {
+  if (list.length <= 1) {
+    return "List has only one item, already sorted";
+  }
+
   list.sort();
+
 }
-function reverse(){
-  list.reverse();
-}
-function count(){
-  const count = result.filter(item => item === params[0]).length;
+
+function countOccurrences(searchString) {
+  let count = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === searchString) {
+      count++;
+    }
+  }
   return count;
+}
+
+function reverseList() {
+  list.reverse();
+ 
 }
